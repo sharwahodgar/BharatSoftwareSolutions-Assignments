e156b076992bc5155a3084a8bbae7efc250f11a1
sharwahodgar <sharwahodgar2030@gmail.com>
2026-09-01 21:26:34 +0530

    Assignment 2

--- SQL_Assignments/Assignment 2.sql
+++ SQL_Assignments/Assignment 2.sql
@@ -9,3 +9,13 @@ select substring(FIRST_NAME, 1, 3) as FIRST_NAME FROM Worker;
 SELECT INSTR(FIRST_NAME, 'a') AS Position
 FROM Worker
 WHERE FIRST_NAME = 'Amitabh';
+
+-- Q-3. Write an SQL query to print the name of employees having the highest salary in each department.
+
+SELECT FIRST_NAME, LAST_NAME, DEPARTMENT, SALARY
+FROM Worker w
+WHERE SALARY = (
+    SELECT MAX(SALARY)
+    FROM Worker
+    WHERE DEPARTMENT = w.DEPARTMENT
+);
