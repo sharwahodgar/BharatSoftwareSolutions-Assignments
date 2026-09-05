15aebc9f65953bf92d628fe31dc9595e5117c513
sharwahodgar <sharwahodgar2030@gmail.com>
2026-09-05 14:33:31 +0530

    py code 13

---
+++ Python_Assignments/13. div by 3 and 6.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number to check if it is divisible by 3 and 6: "))
+
+if num % 3 ==0 and num % 6 ==0:
+    print(num,"is divisible by 3 and 6")
+          
+else:
+    print(num,"is not divisible by 3 and 6")          
