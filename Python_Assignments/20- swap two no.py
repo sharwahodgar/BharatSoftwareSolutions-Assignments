fa77f6c99022d7c1b38380c5920973dcba8ad021
sharwahodgar <sharwahodgar2030@gmail.com>
2026-09-06 22:23:22 +0530

    py code 20

---
+++ Python_Assignments/20- swap two no.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping:")
+print("Your 'a' is: ", a)
+print("Your 'b' is: ", b)
